e96cfec574c08851eb828b336bff73484b920f63
javohir <javohir@example.com>
2023-06-01 21:43:14 +0300

    bi

--- java.js
+++ java.js
@@ -2,28 +2,44 @@
 
             var name = +prompt('  7 + 7 = кошинг')
 
-            if(name == 14){
+
+            if(isNaN(name)){
+                alert('плусга сон критмадингиз ')
+            }
+            else if(name == 14){
             alert("жавоб тогри");
             }else{
             alert("жавоб нотогри кайтадан уриниб коринг");
             }
                                     var name = +prompt('  7 - 7 = айиринг')
 
-                                    if(name == 0){
+                                    if(isNaN(name)){
+                                        alert('минусга сон критмадингиз')
+                                    }
+
+                                    else if(name == 0){
                                     alert("жавоб тогри");
                                     }else{
                                     alert("жавоб нотогри кайтадан уриниб коринг");
                                     }
             var name = +prompt('  7 * 7 = копайтиринг')
 
-            if(name == 49){
+            if(isNaN(name)){
+                alert('копайтиришга сон критмадингиз')
+            }
+
+            else if(name == 49){
             alert("жавоб тогри");
             }else{
             alert("жавоб нотогри кайтадан уриниб коринг");
             }
                                     var name = +prompt('  7 / 7 = болинг')
 
-                                    if(name == 1){
+                                    if(isNaN(name)){
+                                        alert('болишга сон критмадингиз')
+                                    }
+
+                                    else if(name == 1){
                                     alert("жавоб тогри");
                                     }else{
                                     alert("жавоб нотогри кайтадан уриниб коринг");
